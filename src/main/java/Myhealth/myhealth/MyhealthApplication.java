e75b7467ace226358fd59aaf7d682174f45d6799
nurdev1 <fatoudevweb1@gmail.com>
2023-01-06 13:45:49 +0000

    first commit

---
+++ src/main/java/Myhealth/myhealth/MyhealthApplication.java
@@ -0,0 +1,13 @@
+package Myhealth.myhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Myhealth/myhealth/MyhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package Myhealth.myhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
